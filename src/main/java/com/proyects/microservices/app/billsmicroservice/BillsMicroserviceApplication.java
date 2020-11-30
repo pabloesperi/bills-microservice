a4b0da8c35a28956978959c1343e625aed983e1d
pabloesperi <pabloesperi@hotmail.com>
2020-11-30 14:14:12 +0100

    First commit

--- src/main/java/com/proyects/microservices/app/billsmicroservice/BillsMicroserviceApplication.java
+++ src/main/java/com/proyects/microservices/app/billsmicroservice/BillsMicroserviceApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 @SpringBootApplication
 @EntityScan({"com.proyects.microservices.app.commonsmicroservice.models"})
 public class BillsMicroserviceApplication {
- 
+  
 	public static void main(String[] args) {
 		SpringApplication.run(BillsMicroserviceApplication.class, args);
 	}
